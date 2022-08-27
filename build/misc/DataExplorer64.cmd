ea5eb9d6aff7edd5ea0cbd3a1dd5d29dc0b2db4d
brueg <winfried.bruegmann@web.de>
2022-08-27 18:18:05 +0200

    prepare binary build Windows 64 using self contained JRE

--- build/misc/DataExplorer64.cmd
+++ build/misc/DataExplorer64.cmd
@@ -1,3 +1,8 @@
-@REM ***** script to launch DataExplorer ** 09 Apr 2019 *****
+@REM ***** script to launch DataExplorer ** 27 Aug 2022 *****
+
+@REM add path to self contained JRE, require to be exec from installation directory
+@set PATH=%cd%\runtime\bin;%PATH%
+@echo %PATH%
+
 @echo java -Dfile.encoding=UTF-8 -jar -Xms64m -Xmx3092m ./DataExplorer.jar
 @java -Dfile.encoding=UTF-8 -jar -Xms64m -Xmx3092m ./DataExplorer.jar
