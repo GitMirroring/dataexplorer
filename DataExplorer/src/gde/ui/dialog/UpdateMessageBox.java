dd1a5cfeeba255b81f7b9956915b265833e6e9bc
brueg <brueg@raspberrypi>
2024-11-18 15:45:33 +0100

    update layout of update dialog

--- DataExplorer/src/gde/ui/dialog/UpdateMessageBox.java
+++ DataExplorer/src/gde/ui/dialog/UpdateMessageBox.java
@@ -21,9 +21,10 @@ package gde.ui.dialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.FormAttachment;
-import org.eclipse.swt.layout.FormData;
-import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.layout.RowData;
+import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.program.Program;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
@@ -44,6 +45,22 @@ public class UpdateMessageBox extends Dialog {
 	protected Shell		shell;
 
 	/**
+	* Auto-generated main method to display this
+	* org.eclipse.swt.widgets.Dialog inside a new Shell.
+	*/
+	public static void main(String[] args) {
+		try {
+			Display display = Display.getDefault();
+			Shell shell = new Shell(display);
+			UpdateMessageBox inst = new UpdateMessageBox(shell, SWT.NULL);
+			inst.open();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
 	 * Create the dialog.
 	 * @param parent
 	 * @param style
@@ -60,7 +77,8 @@ public class UpdateMessageBox extends Dialog {
 	public boolean open() {
 		createContents();
 		shell.open();
-		shell.layout();
+		//shell.layout();
+		shell.pack();
 		shell.setLocation(500, 300);
 		Display display = getParent().getDisplay();
 		while (!shell.isDisposed()) {
@@ -76,53 +94,45 @@ public class UpdateMessageBox extends Dialog {
 	 */
 	private void createContents() {
 		shell = new Shell(getParent(), SWT.BORDER | SWT.TITLE);
-		shell.setSize(350, 250);
+		//shell.setSize(400, 250);
 		shell.setText(getText());
-		shell.setLayout(new FormLayout());
+		RowLayout rowLayout = new RowLayout(SWT.VERTICAL);
+		rowLayout.center = true;
+		rowLayout.marginLeft = 20;
+    rowLayout.marginTop = 20;
+    rowLayout.marginRight = 20;
+    rowLayout.marginBottom = 20;
+		shell.setLayout(rowLayout);
 		
 		Text txttNewUpdateAvail = new Text(shell, SWT.CENTER);
-		FormData fd_txttNewUpdateAvail = new FormData();
-		fd_txttNewUpdateAvail.right = new FormAttachment(100, -5);
-		fd_txttNewUpdateAvail.top = new FormAttachment(0, 15);
-		fd_txttNewUpdateAvail.left = new FormAttachment(0, 5);
-		fd_txttNewUpdateAvail.height = 25;
-		txttNewUpdateAvail.setLayoutData(fd_txttNewUpdateAvail);
+		txttNewUpdateAvail.setLayoutData(new RowData(370, 30));
 		txttNewUpdateAvail.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_WIDGET_BACKGROUND));
 		txttNewUpdateAvail.setEditable(false);
 		txttNewUpdateAvail.setFont(SWTResourceManager.getFont(GDE.WIDGET_FONT_NAME, GDE.WIDGET_FONT_SIZE, SWT.NORMAL));
 		txttNewUpdateAvail.setText(Messages.getString(MessageIds.GDE_MSGI0072));
 		
 		Text txtDownloadProblemHint = new Text(shell, SWT.WRAP | SWT.CENTER | SWT.MULTI);
-		FormData fd_txtDownloadProblemHint = new FormData();
-		fd_txtDownloadProblemHint.right = new FormAttachment(100, -20);
-		fd_txtDownloadProblemHint.top = new FormAttachment(0, 50);
-		fd_txtDownloadProblemHint.left = new FormAttachment(0, 20);
-		fd_txtDownloadProblemHint.height = 30;
-		txtDownloadProblemHint.setLayoutData(fd_txtDownloadProblemHint);
+		txtDownloadProblemHint.setLayoutData(new RowData(370, 60));
 		txtDownloadProblemHint.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_WIDGET_BACKGROUND));
 		txtDownloadProblemHint.setEditable(false);
 		txtDownloadProblemHint.setFont(SWTResourceManager.getFont(GDE.WIDGET_FONT_NAME, GDE.WIDGET_FONT_SIZE, SWT.NORMAL));
 		txtDownloadProblemHint.setText(Messages.getString(MessageIds.GDE_MSGI0073));
 		
 		Text txtBetaVersionHint = new Text(shell, SWT.WRAP | SWT.CENTER | SWT.MULTI);
-		FormData fd_txtBetaVersionHint = new FormData();
-		fd_txtBetaVersionHint.right = new FormAttachment(100, -20);
-		fd_txtBetaVersionHint.top = new FormAttachment(0, 90);
-		fd_txtBetaVersionHint.left = new FormAttachment(0, 20);
-		fd_txtBetaVersionHint.height = 30;
-		txtBetaVersionHint.setLayoutData(fd_txtBetaVersionHint);
+		txtBetaVersionHint.setLayoutData(new RowData(370, 70));
 		txtBetaVersionHint.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_WIDGET_BACKGROUND));
 		txtBetaVersionHint.setFont(SWTResourceManager.getFont(GDE.WIDGET_FONT_NAME, GDE.WIDGET_FONT_SIZE, SWT.NORMAL));
 		txtBetaVersionHint.setText(Messages.getString(MessageIds.GDE_MSGI0074));
 		txtBetaVersionHint.setEditable(false);
 		
-		Link linkNews = new Link(shell, SWT.NONE);
-		FormData fd_linkNews = new FormData();
-		fd_linkNews.top = new FormAttachment(0, 140);
-		fd_linkNews.left = new FormAttachment(0, 20);
-		fd_linkNews.height = 25;
-		fd_linkNews.width = 180;
-		linkNews.setLayoutData(fd_linkNews);
+		Composite composite = new Composite(shell, SWT.NONE);
+		composite.setLayoutData(new RowData(370, 50));
+		FillLayout filllayout = new FillLayout(SWT.HORIZONTAL);
+		filllayout.marginWidth = 25;
+		filllayout.spacing = 50;
+		composite.setLayout(filllayout);
+
+		Link linkNews = new Link(composite, SWT.NONE);
 		linkNews.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_WIDGET_BACKGROUND));
 		linkNews.setFont(SWTResourceManager.getFont(GDE.WIDGET_FONT_NAME, GDE.WIDGET_FONT_SIZE, SWT.NORMAL));
 		linkNews.setText("<a>" + Messages.getString(MessageIds.GDE_MSGI0075) + "</a>");
@@ -133,13 +143,7 @@ public class UpdateMessageBox extends Dialog {
 			}
 		});
 		
-		Link linkDonate = new Link(shell, SWT.NONE);
-		FormData fd_linkDonate = new FormData();
-		fd_linkDonate.right = new FormAttachment(100, -20);
-		fd_linkDonate.top = new FormAttachment(0, 140);
-		fd_linkDonate.width = 80;
-		fd_linkDonate.height = 25;
-		linkDonate.setLayoutData(fd_linkDonate);
+		Link linkDonate = new Link(composite, SWT.NONE);
 		linkDonate.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_WIDGET_BACKGROUND));
 		linkDonate.setFont(SWTResourceManager.getFont(GDE.WIDGET_FONT_NAME, GDE.WIDGET_FONT_SIZE, SWT.NORMAL));
 		linkDonate.setText("<a>" + Messages.getString(MessageIds.GDE_MSGI0076) + "</a>");
@@ -149,22 +153,16 @@ public class UpdateMessageBox extends Dialog {
 				Program.launch("https://www.paypal.com/paypalme/WinfriedBruegmann");
 			}
 		});
-	
-		Composite composite = new Composite(shell, SWT.NONE);
-		FormData fd_composite = new FormData();
-		fd_composite.right = new FormAttachment(100, -5);
-		fd_composite.bottom = new FormAttachment(100, -5);
-		fd_composite.left = new FormAttachment(0, 5);
-		fd_composite.height = 50;
-		composite.setLayoutData(fd_composite);
-		composite.setLayout(new FormLayout());
+		composite.layout();
 		
-		Button btnNo = new Button(composite, SWT.CENTER|SWT.PUSH);
-		FormData fd_btnNo = new FormData();
-		fd_btnNo.left = new FormAttachment(0, 50);
-		fd_btnNo.bottom = new FormAttachment(100, -10);
-		fd_btnNo.width = 100;
-		btnNo.setLayoutData(fd_btnNo);
+		Composite composite2 = new Composite(shell, SWT.NONE);
+		composite2.setLayoutData(new RowData(370, 40));
+		FillLayout fLayout = new FillLayout(SWT.HORIZONTAL);
+		fLayout.marginWidth = 20;
+		fLayout.spacing = 50;
+		composite2.setLayout(fLayout);
+				
+		Button btnNo = new Button(composite2, SWT.CENTER|SWT.PUSH);
 		btnNo.setFont(SWTResourceManager.getFont(GDE.WIDGET_FONT_NAME, GDE.WIDGET_FONT_SIZE, SWT.NORMAL));
 		btnNo.setText(Messages.getString(MessageIds.GDE_MSGI0078));
 		btnNo.addSelectionListener(new SelectionAdapter() {
@@ -174,12 +172,7 @@ public class UpdateMessageBox extends Dialog {
 			}
 		});
 		
-		Button btnYes = new Button(composite, SWT.CENTER|SWT.PUSH);
-		FormData fd_btnYes = new FormData();
-		fd_btnYes.right = new FormAttachment(100, -50);
-		fd_btnYes.bottom = new FormAttachment(100, -10);
-		fd_btnYes.width = 100;
-		btnYes.setLayoutData(fd_btnYes);
+		Button btnYes = new Button(composite2, SWT.CENTER|SWT.PUSH);
 		btnYes.setFont(SWTResourceManager.getFont(GDE.WIDGET_FONT_NAME, GDE.WIDGET_FONT_SIZE, SWT.NORMAL));
 		btnYes.setText(Messages.getString(MessageIds.GDE_MSGI0077));
 		btnYes.addSelectionListener(new SelectionAdapter() {
@@ -189,5 +182,6 @@ public class UpdateMessageBox extends Dialog {
 				shell.dispose();
 			}
 		});
+		composite2.layout();
 	}
 }
