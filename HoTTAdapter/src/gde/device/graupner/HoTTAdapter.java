3fab5fa3c115ce17f8e170f149db2347884cb9fb
brueg <winfried.bruegmann@web.de>
2025-04-05 11:57:08 +0200

    update MDL viewer plug-in to 1408 and add dark theme support

--- HoTTAdapter/src/gde/device/graupner/HoTTAdapter.java
+++ HoTTAdapter/src/gde/device/graupner/HoTTAdapter.java
@@ -2423,6 +2423,7 @@ public class HoTTAdapter extends DeviceConfiguration implements IDevice, IHistoD
 					CTabItem mdlTabItem = HoTTAdapter.this.getMdlTabItem();
 					if (mdlTabItem != null) {
 						DataExplorer.getInstance().registerCustomTabItem(mdlTabItem);
+						mdlTabItem.getControl().setBackground(Settings.getInstance().getCompareSurroundingBackground());
 					}
 				}
 			}
