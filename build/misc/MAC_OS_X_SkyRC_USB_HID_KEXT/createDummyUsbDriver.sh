7937ca495a3a91c06c8a23e139a54e8efa714028
brueg <winfried.bruegmann@web.de>
2025-11-30 15:01:55 +0100

    update Readme's to provide info about macOS kernel extension

--- build/misc/MAC_OS_X_SkyRC_USB_HID_KEXT/createDummyUsbDriver.sh
+++ build/misc/MAC_OS_X_SkyRC_USB_HID_KEXT/createDummyUsbDriver.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
-# ***** script to install user space kernel extension to make SkyRC USB-HID adapter usable with MAC OS X ** 06 Mar 2019 WB *****
-# execution: sudo ./createDummyUsbDriver.sh
-cp -r SkyRC.kext /Library/Extensions/
-cd /Library/Extensions
-chown -R root:wheel SkyRC.kext
-chmod -R 755 SkyRC.kext
-kextcache -system-caches
+# ***** Info about kernel extension to make SkyRC USB-HID adapter usable with MAC OS X ** 30 Nov 2025 WB *****
 echo The kernel extension does not contain any code, it only prevent system to allocate USB HID and enable permission for user context.
-echo After SkyRC USB kernel extension installed please reboot the system before connecting a SKyRC USB device again.
-echo MAC OS X >= 10.13 High Sierra kernel extensions might be blocked, reboot into recovery console "(push cmd+R while boot)" open terminal
-echo Enter "spctl kext-consent disable" and "csrutil disable", reboot to activate
-echo Open terminal enter "sudo kextutil -t -v 1 /Library/Extensions/SkyRC.kext" each time before USB cable connected. 
-echo This will load none signed kext, ignore warnings regarding missing code signing error 
-echo Use /Library/Extensions directory if /System/Library/Extensions is read-only file system "(Catalina)" or "sudo mount -rw /"
-echo Alternative use Kext Utility or KextBeast to copy SkyRC.kext os search the Internet for installing 3rd party kext
-echo Kext Utiliy may help installing kernel extension, search internet for download link
-echo Unfortunately, "kext-dev-mode=1" and "rootless=0" these keys are no longer working for future versions of Mac OS X starting with El Capitan GM. 
-echo You need disable SIP with Clover Bootloader.
-echo To run Kext Utility, you need to run in terminal this command "sudo spctl --master-disable"
-echo After starting Kext Utility just drag and drop the kernel extension complete directory tree to let it install
+echo After iCharger kernel extension installed please reboot the system before connecting iCharger with USB-HID again.
+echo MAC OS X >= 10.13 High Sierra kernel extensions might be blocked
+echo To enable kext on MacOS Sonoma and newer check https://github.com/chris1111/Kext-Droplet-macOS.
