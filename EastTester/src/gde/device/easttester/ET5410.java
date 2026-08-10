e04243576d870551acff903d8dce3602198e1260
Winfried Brügmann <winfried.bruegmann@web.de>
2026-08-10 16:04:04 +0200

    fix energy calculation ET5410

--- EastTester/src/gde/device/easttester/ET5410.java
+++ EastTester/src/gde/device/easttester/ET5410.java
@@ -164,8 +164,8 @@ public class ET5410 extends DeviceConfiguration implements IDevice {
 			energySum = 0.;
 			break;
 		default: //add up energy
-			energySum += Double.valueOf((points[0] / 1000.0 * points[1] / 1000.0) / 360.0 + 0.502);
-			points[4] = energySum.intValue();
+			energySum += (points[0] / 1000.0 * points[1] / 1000.0) / 3600.;
+			points[4] = (int) (energySum * 1000.0);
 			break;
 		}
 		
