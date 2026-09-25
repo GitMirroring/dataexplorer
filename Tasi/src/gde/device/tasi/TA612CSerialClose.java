2a5625d39eb46db21c13fb59f6b67938578e584f
Winfried Brügmann <winfried.bruegmann@web.de>
2026-09-25 18:13:05 +0200

    update copyright banner

--- Tasi/src/gde/device/tasi/TA612CSerialClose.java
+++ Tasi/src/gde/device/tasi/TA612CSerialClose.java
@@ -1,6 +1,23 @@
-/* SPDX-License-Identifier: GPL-3.0-or-later
- * refer to https://github.com/oikumene-works/tasi/tree/main/ta612c/software/dataexplorer-plugin
- */
+/**************************************************************************************
+  	This file is part of GNU DataExplorer.
+
+    GNU DataExplorer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DataExplorer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GNU DataExplorer.  If not, see <https://www.gnu.org/licenses/>.
+
+    Copyright (c) 2026 Jyri Wennström, Oikumene Works
+    refer to https://github.com/oikumene-works/tasi/tree/main/ta612c/software/dataexplorer-plugin
+    Copyright (c) 2026 Winfried Bruegmann
+****************************************************************************************/
 package gde.device.tasi;
 
 import gde.comm.IDeviceCommPort;
